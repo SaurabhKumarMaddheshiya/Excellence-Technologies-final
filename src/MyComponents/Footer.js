2e29269573ab8d8cd612280587a6b06e9cc6a041
SaurabhKumarMaddheshiya <er.saurabhmaddheshiya@gmail.com>
2021-05-27 19:28:59 +0530

    done todo app

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./footer.css"
+
+export default function Footer() {
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center footer">
+            Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
+}
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TodoItems from './TodoItems';
+
+const Todos = (props) => {
+    return (
+        <div className="container my-3">
+            <h3 className="my-3">Todos List</h3>
+            {props.todos.length === 0 ? "No Todos to display" :
+            props.todos.map((todo) => {
+                return ( <TodoItems todo={todo} key={todo.sno} onDelete = {props.onDelete}/>)
+            })
+             }
+        </div>
+    )
+}
+
+export default Todos;
